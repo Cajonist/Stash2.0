c8f440735c2a9571f6fd7aa2636e06cde5732eb6
Cajonist <colm.ocallaghan@stmarysonthehill.ie>
2026-07-08 14:21:06 +0100

    Update config.js

--- extension/config.js
+++ extension/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlcWx0cGFsYWxwZnh4anVjZXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1MDEzNTIsImV4cCI6MjA5OTA3NzM1Mn0.8kSJMYMckH1AnS-9jCnSpQ8_Kwe6htaU0Hx-aqIjqtY',
 
   // Your web app URL (after deploying to Vercel/Netlify)
-  WEB_APP_URL: 'https://your-stash-app.vercel.app',
+  WEB_APP_URL: 'https://stash2-0-cajonist17.vercel.app/',
 
   // Your user ID from Supabase (Authentication > Users)
   // For multi-user mode, this can be removed and auth will be required
