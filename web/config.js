5438bbdb0e48f2e6e10d3d0b6e4e3d5d6f688ff8
Cajonist <colm.ocallaghan@stmarysonthehill.ie>
2026-07-08 16:48:01 +0100

    Update config.js

--- web/config.js
+++ web/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   SUPABASE_URL: 'https://aeqltpalalpfxxjucere.supabase.co/rest/v1/',
 
   // Your Supabase anon/public key (from Project Settings > API)
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlcWx0cGFsYWxwZnh4anVjZXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1MDEzNTIsImV4cCI6MjA5OTA3NzM1Mn0.8kSJMYMckH1AnS-9jCnSpQ8_Kwe6htaU0Hx-aqIjqtY',
 
   // Your user ID from Supabase (Authentication > Users)
   // For multi-user mode, this can be removed and auth will be required
